6a60d6322d9d5e899875ed4b180e178b0bc63d7d
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 19:33:24 +0700

    thêm ảnh chạy code bài 15

--- bài 15/Program.cs
+++ bài 15/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 
-namespace bài_14
+namespace bài_15
 {
     internal class Program
     {
@@ -8,25 +8,24 @@ namespace bài_14
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            Dictionary<string, string> tuDien = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            Dictionary<string, string> tuDien = new Dictionary<string, string>();
             tuDien.Add("Hello", "Xin chào");
             tuDien.Add("World", "Thế giới");
             tuDien.Add("Student", "Học sinh, sinh viên");
             tuDien.Add("Teacher", "Giáo viên");
             tuDien.Add("Programming", "Lập trình");
             tuDien.Add("Computer", "Máy tính");
-            Console.WriteLine("--- TỪ ĐIỂN ANH - VIỆT ---");
-            Console.WriteLine("Nhập từ tra: ");
-            while (true) { 
+            Console.WriteLine("--- TỪ ĐIỂN ANH - VIỆT ĐƠN GIẢN ---");
+            Console.WriteLine("Nhập từ cần tra: ");
+            while (true)
+            {
                 string word = Console.ReadLine();
                 if (tuDien.ContainsKey(word))
-                {
                     Console.WriteLine($"Nghĩa của từ {word}: {tuDien[word]}");
-                }
                 else
-                {
-                    Console.WriteLine($"Từ chưa được cập nhật");
-                }
+                    Console.WriteLine($"Từ điểm chưa được cập nhật");
             }
         }
     }
