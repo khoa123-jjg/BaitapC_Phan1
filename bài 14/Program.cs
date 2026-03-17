b07c53cdae538f9da9df73696e4726c18dc80414
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 19:37:02 +0700

    bài 14 thêm ảnh code

--- bài 14/Program.cs
+++ bài 14/Program.cs
@@ -1,30 +1,53 @@
 ﻿using System.Text;
 
-namespace bài_15
+namespace bài_14
 {
+    class Student
+    {
+        public string Name;
+        public int Id;
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void In()
+        {
+            Console.WriteLine($"{Id} | {Name}");
+        }
+    }
     internal class Program
     {
+        static void xoa(List<Student> list, int ID)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Id == ID)
+                {
+                    list.Remove(list[i]);
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            Dictionary<string, string> tuDien = new Dictionary<string, string>();
-            tuDien.Add("Hello", "Xin chào");
-            tuDien.Add("World", "Thế giới");
-            tuDien.Add("Student", "Học sinh, sinh viên");
-            tuDien.Add("Teacher", "Giáo viên");
-            tuDien.Add("Programming", "Lập trình");
-            tuDien.Add("Computer", "Máy tính");
-            Console.WriteLine("--- TỪ ĐIỂN ANH - VIỆT ĐƠN GIẢN ---");
-            Console.WriteLine("Nhập từ cần tra: ");
-            while (true)
-            {
-                string word = Console.ReadLine();
-                if (tuDien.ContainsKey(word))
-                    Console.WriteLine($"Nghĩa của từ {word}: {tuDien[word]}");
-                else
-                    Console.WriteLine($"Từ điểm chưa được cập nhật");
-            }
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<Student> list = new List<Student>();
+            list.Add(new Student("Nguyen Van A", 1));
+            list.Add(new Student("Nguyen Van B", 2));
+            list.Add(new Student("Nguyen Van C", 3));
+            Console.WriteLine("--- DANH SÁCH SINH VIÊN ---");
+            foreach (Student student in list)
+                student.In();
+            Console.WriteLine("Nhập ID sinh viên cần xóa:");
+            int id = int.Parse(Console.ReadLine());
+            xoa(list, id);
+            Console.WriteLine("--- DANH SÁCH SINH VIÊN ---");
+            foreach (Student student in list)
+                student.In();
+            Console.ReadKey();
         }
     }
 }
