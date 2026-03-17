c33ad26cda30b0a66b3efa89cfe59889be221cdc
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:03:47 +0700

    bài tập 8 ver1

---
+++ bài 8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace bài_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<int> list = new List<int> { 1,2,3,4,5,6};
+            Console.WriteLine($"Các phần tử ban đầu trong list là: " + string.Join(",", list));
+            list.Reverse();
+            Console.WriteLine("\n---------------------------------\n");
+            Console.WriteLine($"Các phần tử sau khi đảo ngược trong list là: " + string.Join(",", list));
+            Console.ReadKey();
+
+        }
+    }
+}
