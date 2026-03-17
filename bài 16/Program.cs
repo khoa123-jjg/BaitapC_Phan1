af6c02b3da8b381ef92774ab37b7a021e99c0d0b
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:16:13 +0700

    bài tập 16 ver1

---
+++ bài 16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace bài_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.Write("Nhập vào một chuỗi: ");
+            string word = Console.ReadLine();
+            Dictionary<char,int> tuDien = new Dictionary<char,int>();
+            foreach (string k in word.Split(' '))
+            {
+                foreach (char y in k)
+                {
+                    if (tuDien.ContainsKey(y))
+                    {
+                        tuDien[y]++;
+                    }
+                    else
+                    {
+                        tuDien.Add(y, 1);
+                    }
+                }
+            }
+            foreach (var z in tuDien )
+            {
+                Console.Write($"{z.Key}:{z.Value} ");
+            }
+        }
+    }
+}
