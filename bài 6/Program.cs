3be4253ed1cfbc83db7cfad202d7711e3ae9ed92
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:01:27 +0700

    bài 6 ver 1

---
+++ bài 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace bài_6
+{
+    internal class Program
+    {
+        static void In(List<string> list)
+        {
+            Console.WriteLine("Danh sách hiện có ("+string.Join(",",list)+")");
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<string> list = new List<string> { "Táo", "Cam", "xoài", "bưởi" };
+            Console.WriteLine("--- Danh sách chuỗi ---");
+            In(list);
+            string name = null;
+            Console.Write("Nhập chuỗi cần tìm: ");
+            name = Console.ReadLine();
+            if (list.Contains(name))
+                Console.WriteLine("Chuỗi " + name + " có tồn tại trong danh sách.");
+            else 
+                {
+                Console.WriteLine("Chuỗi " + name + " không tồn tại trong danh sách.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
