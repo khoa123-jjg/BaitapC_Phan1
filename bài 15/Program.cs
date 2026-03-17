12ea57726aa14ff6d9aba65be543e09ca6cb4a3e
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:14:36 +0700

    bài tập 15 ver1

---
+++ bài 15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace bài_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Dictionary<string, string> tuDien = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            tuDien.Add("Hello", "Xin chào");
+            tuDien.Add("World", "Thế giới");
+            tuDien.Add("Student", "Học sinh, sinh viên");
+            tuDien.Add("Teacher", "Giáo viên");
+            tuDien.Add("Programming", "Lập trình");
+            tuDien.Add("Computer", "Máy tính");
+            Console.WriteLine("--- TỪ ĐIỂN ANH - VIỆT ---");
+            Console.WriteLine("Nhập từ tra: ");
+            while (true) { 
+                string word = Console.ReadLine();
+                if (tuDien.ContainsKey(word))
+                {
+                    Console.WriteLine($"Nghĩa của từ {word}: {tuDien[word]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Từ chưa được cập nhật");
+                }
+            }
+        }
+    }
+}
