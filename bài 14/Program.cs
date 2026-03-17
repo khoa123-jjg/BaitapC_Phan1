7a6bd64b42b0aec245b15db800544632cbc391d1
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:13:44 +0700

    bài tập 14 ver1

---
+++ bài 14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace bài_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Dictionary<string, string> tuDien = new Dictionary<string, string>();
+            tuDien.Add("Hello", "Xin chào");
+            tuDien.Add("World", "Thế giới");
+            tuDien.Add("Student", "Học sinh, sinh viên");
+            tuDien.Add("Teacher", "Giáo viên");
+            tuDien.Add("Programming", "Lập trình");
+            tuDien.Add("Computer", "Máy tính");
+            Console.WriteLine("--- TỪ ĐIỂN ANH - VIỆT ĐƠN GIẢN ---");
+            Console.WriteLine("Nhập từ cần tra: ");
+            while (true)
+            {
+                string word = Console.ReadLine();
+                if (tuDien.ContainsKey(word))
+                    Console.WriteLine($"Nghĩa của từ {word}: {tuDien[word]}");
+                else
+                    Console.WriteLine($"Từ điểm chưa được cập nhật");
+            }
+        }
+    }
+}
