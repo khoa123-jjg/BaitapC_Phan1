c4b6f7740824bb5fd8230d069312ef973b62916d
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 18:08:37 +0700

    thêm ảnh code chạy bài 10

--- bài 10/Program.cs
+++ bài 10/Program.cs
@@ -8,6 +8,8 @@ namespace bài_10
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
             List<int> list = new List<int>();
             Console.Write("Nhập số lượng phần tử của list:");
             int n = int.Parse(Console.ReadLine());
