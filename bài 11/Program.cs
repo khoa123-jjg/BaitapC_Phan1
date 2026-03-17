06aee93ab836117ee8dc78d8ad171f21ead03674
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:09:55 +0700

    bài tập 11 ver1

---
+++ bài 11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace bài_11
+{
+    class Student
+    {
+        public string Name;
+        public int Id;
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void In()
+        {
+            Console.WriteLine($"{Id} | {Name}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<Student> list = new List<Student>();
+            list.Add(new Student("Nguyen Van A", 1));
+            list.Add(new Student("Nguyen Van B", 2));
+            list.Add(new Student("Nguyen Van C", 3));
+            Console.WriteLine("--- DANH SÁCH SINH VIÊN ---");
+            foreach (Student student in list)
+                student.In();
+
+        }
+    }
+}
