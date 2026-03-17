9b77b5cbabbd1f65499b15ca49ec1d9a7026db6e
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 12:02:45 +0700

    bài tập 7 ver1

---
+++ bài 7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace bài_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<int> list = new List<int>();
+            Console.Write("Nhập số lượng phần tử của list:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n---------------------------------\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhập phần tử thứ [" + (i + 1) + "]: ");
+                int k = int.Parse(Console.ReadLine());
+                list.Add(k);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n-1; j >= 1; j--) {
+                    if (list[j] > list[j - 1])
+                    {
+                        int k;
+                        k = list[j];
+                        list[j] = list[j - 1];
+                        list[j - 1] = k;
+                    }
+                }
+            }
+            Console.WriteLine("\n---------------------------------\n");
+            Console.WriteLine($"\nCác phần tử sau khi xắp xếp trong list là: "+string.Join(",",list));
+            Console.ReadKey();
+
+        }
+    }
+}
