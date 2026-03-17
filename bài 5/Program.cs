fe980b436a4f209958a780a24ef5872010b59682
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 17:10:28 +0700

    Thêm ảnh code chạy

--- bài 5/Program.cs
+++ bài 5/Program.cs
@@ -1,37 +1,34 @@
 ﻿using System.Text;
 
-namespace bài_5
+namespace bài_4
 {
-
     internal class Program
     {
-        static void In(List<string> list)
-        {
-            Console.WriteLine("Danh sách sinh viên:");
-            foreach (string k in list)
-            {
-                Console.Write(k + "|");
-            }
-        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            List<string> list = new List<string> { "Nguyễn Văn An", "Lê Thị Bình", "Trần Hoàng Chi", "Phạm Quốc Dũng" };
-            Console.WriteLine("--- Danh sách sinh viên hiện tại ---");
-            In(list);
-            string name = null;
-            do
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
+            List<float> list = new List<float>();
+            Console.Write("Nhập số lượng phần tử của list:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n---------------------------------\n");
+            for (int i = 0; i < n; i++)
             {
-                Console.Write($"Không tìm thấy sinh viên có tên {name} trong danh sách");
-                Console.Write("\nNhập tên sinh viên cần xóa:");
-                name = Console.ReadLine();
-            } while (list.Remove(name) == false);
-            Console.WriteLine($"Đã xóa sinh viên có tên {name} khỏi danh sách.");
-            Console.WriteLine("\n--- Danh sách sinh viên sau khi cập nhật ---");
-            In(list);
+                Console.Write("Nhập phần tử thứ [" + (i + 1) + "]: ");
+                float k = float.Parse(Console.ReadLine());
+                list.Add(k);
+            }
+            float count = 0;
+            foreach (float k in list)
+            {
+                if (k % 2 == 0)
+                    count++; 
+            }
+            Console.WriteLine("\n---------------------------------\n");
+            Console.WriteLine($"\nSố lượng phần tử chẵn trong list là: {count}");
             Console.ReadKey();
-
 
         }
     }
