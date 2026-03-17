eb02966d75ce2d8c6a12f3c6ef2d418e70e73c71
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 11:35:22 +0700

    Bài tập 1 ver1

---
+++ bài 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace bài_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            Console.WriteLine("Các phần tử trong list:");
+            foreach (int k in list)
+            {
+                Console.WriteLine(""+k);
+            }
+            Console.ReadKey();
+        }
+    }
+}
