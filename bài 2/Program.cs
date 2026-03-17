efbef34d9a71a7a296d2df4754b81f9acd53c9b3
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 11:51:17 +0700

    bài 2 ver1

---
+++ bài 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace bài_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            List<float> list = new List<float>();
+            Console.Write("Nhập số lượng phần tử của list:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n---------------------------------\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhập phần tử thứ [" + (i + 1) + "]: ");
+                float k = float.Parse(Console.ReadLine());
+                list.Add(k);
+            }
+            float tong = 0;
+            foreach (float k in list)
+            {
+                tong += k;
+            }
+            Console.WriteLine("\n---------------------------------\n");
+            Console.WriteLine($"\nTổng các phần tử trong list là: {tong}");
+            Console.ReadKey();
+
+        }
+    }
+}
