4f68e53775a287f242d914f025ce29692d313166
Khoa123-jjg <khoapham.12082006@gmail.com>
2026-03-17 18:29:40 +0700

    Thêm ảnh chạy code bài 11

--- bài 11/Program.cs
+++ bài 11/Program.cs
@@ -22,6 +22,8 @@ namespace bài_11
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phạm Nhật Khoa");
+            Console.WriteLine("2415053122221");
             List<Student> list = new List<Student>();
             list.Add(new Student("Nguyen Van A", 1));
             list.Add(new Student("Nguyen Van B", 2));
@@ -29,6 +31,7 @@ namespace bài_11
             Console.WriteLine("--- DANH SÁCH SINH VIÊN ---");
             foreach (Student student in list)
                 student.In();
+            Console.ReadKey();
 
         }
     }
